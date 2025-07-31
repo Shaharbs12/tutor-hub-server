d0b91a29fcfc24a8666073d54984f876c7845841
shaharbs <shaharbs524@gmail.com>
2025-07-31 19:14:50 +0300

    update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from client directory
-app.use(express.static(path.join(__dirname, '../client')));
+//app.use(express.static(path.join(__dirname, '../client')));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
@@ -31,9 +31,9 @@ app.use('/api/admin', require('./routes/admin'));
 // app.use('/api/students', require('./routes/students'));
 
 // Root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
+//app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../client/index.html'));
+//});
 
 // API health check
 app.get('/api/health', (req, res) => {
